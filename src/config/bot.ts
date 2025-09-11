0f489cd8a788db0c81a80e4f1b1fdec940f23a35
Binamra Lamsal <binamralamsal@gmail.com>
2025-09-11 22:22:59 +0545

    Update bot.ts

--- src/config/bot.ts
+++ src/config/bot.ts
@@ -3,4 +3,6 @@ import { Bot } from "grammy";
 import type { BotContext } from "../types.ts";
 import { env } from "./env.ts";
 
-export const bot = new Bot<BotContext>(env.BOT_TOKEN);
+export const bot = new Bot<BotContext>(env.BOT_TOKEN, {
+  client: { apiRoot: env.CUSTOM_API_ROOT },
+});
